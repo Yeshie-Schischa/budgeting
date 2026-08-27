6e0446d32526895bfc230757d3ee8b12f14b52c8
Yeshie-Schischa <schischayeshie@gmail.com>
2026-08-27 09:48:14 +0000

    save

--- script.js
+++ script.js
@@ -5,86 +5,127 @@ const listAcounts = document.getElementById("listAcounts")
 const descriptionField = document.getElementById("description")
 const transactionSection = document.getElementById("transactions")
 const ul = transactionSection.querySelector("ul");
-const form = document.getElementById("payment");
+const formPayment = document.getElementById("payment");
 const moveMoneyBtn = document.getElementById("move-money")
-const formWrap = document.getElementById("form-wrap");
-const closeBtn = document.getElementById("close")
-const accountsElement = document.getElementById("accounts")
+const formWrapPayment = document.getElementById("form-wrap-payment");
+const closePaymentBtn = document.getElementById("close-payment-form")
+const accountsElement = document.getElementById("accounts");
+const addAccountBtn = document.getElementById("add-account");
+const formWrapAccount = document.getElementById("form-wrap-account");
+const closeAccountFormBtn = document.getElementById("close-account-form");
+const submitAccountBtn = document.getElementById("submit-account");
+const formAccount = document.getElementById("add-account-form");
+const accountNameInput = document.getElementById("name");
+const errorMsg = document.getElementById("error-msg")
 
 const transactions = [];
 const accounts = [
-    {name: "Shaye Cash",
-     amount: 0
-    },
-    {name: "G rifky Cash",
+  {
+    name: "Shaye Cash",
     amount: 0
-    }
+  },
+  {
+    name: "G rifky Cash",
+    amount: 0
+  }
 ]
-accounts.forEach(account => {
+const displayAccount = () => {
+  accountsElement.innerHTML = ""
+  accounts.forEach(account => {
     accountsElement.innerHTML += `<li>${account.name}${account.amount}</li>`
-})
-function credit(account, amount, description = ""){
-    const newBalance = accounts.find(acc => acc.name === account).amount += amount
-    transactions.push({account, amount, description, type:"credit", newBalance: newBalance})
-    
+  })
+}
+displayAccount()
+function credit(account, amount, description = "") {
+  const newBalance = accounts.find(acc => acc.name === account).amount += amount
+  transactions.push({ account, amount, description, type: "credit", newBalance: newBalance })
+
 }
 // this is a new command
-function debit(account, amount, description = ""){
-    const newBalance = accounts.find(acc => acc.name === account).amount -= amount
-    transactions.push({account, amount, description, type:"debit", newBalance:newBalance})
-    
+function debit(account, amount, description = "") {
+  const newBalance = accounts.find(acc => acc.name === account).amount -= amount
+  transactions.push({ account, amount, description, type: "debit", newBalance: newBalance })
+
 }
 
-credit("G rifky Cash", 1000)
 
-creditButton.addEventListener("click", (e)=>{
+creditButton.addEventListener("click", (e) => {
   e.preventDefault()
-  if(!form.reportValidity()){
+  if (!formPayment.reportValidity()) {
     return
   }
   const newBalance = credit(listAcounts.value, Number(amountField.value), descriptionField.value)
-  ul.insertAdjacentHTML("beforeend", `<li>Amount: ${Number(amountField.value)} Description: ${descriptionField.value} Account: ${listAcounts.value} New Balance: ${transactions[transactions.length-1].newBalance}</li>`)
+  ul.insertAdjacentHTML("beforeend", `<li>Amount: ${Number(amountField.value)} Description: ${descriptionField.value} Account: ${listAcounts.value} New Balance: ${transactions[transactions.length - 1].newBalance}</li>`)
   listAcounts.value = "";
-  amountField.value =  "";
+  amountField.value = "";
   descriptionField.value = "";
 });
 
-debitButton.addEventListener("click", (e)=>{
+debitButton.addEventListener("click", (e) => {
   e.preventDefault()
-  e.preventDefault()
-  if(!form.reportValidity()){
+  if (!formPayment.reportValidity()) {
     return
   }
   const newBalance = debit(listAcounts.value, Number(amountField.value), descriptionField.value)
-  ul.insertAdjacentHTML("beforeend", `<li>Amount: ${Number(amountField.value)} Description: ${descriptionField.value} Account: ${listAcounts.value} New Balance: ${transactions[transactions.length-1].newBalance}</li>`)
+  ul.insertAdjacentHTML("beforeend", `<li>Amount: ${Number(amountField.value)} Description: ${descriptionField.value} Account: ${listAcounts.value} New Balance: ${transactions[transactions.length - 1].newBalance}</li>`)
   listAcounts.value = "";
-  amountField.value =  "";
+  amountField.value = "";
   descriptionField.value = "";
 });
 
+submitAccountBtn.addEventListener("click", (e) => {
+  e.preventDefault()
+  if (!formAccount.reportValidity() || !isValidName) {
+    return
+  }
 
+  accounts.push({ name: accountNameInput.value, amount: 0 })
+  accountNameInput.value = "";
+  displayAccount()
+});
+let isValidName = true
+accountNameInput.addEventListener("input", ()=>{
+  const exist = accounts.some(account =>
+    account.name.toLowerCase().trim() === accountNameInput.value.toLowerCase().trim()
+  )
+  if(exist){
+    errorMsg.innerText = "This account name already exis,please choose another name"
+    isValidName = false
+  } else {
+    errorMsg.innerText = ""
+    isValidName = true
+  }
+})
 
 function displayTransactions() {
-    
-    transactions.forEach(transaction => {
-       ul.innerHTML = `<li>Amount: ${transaction.amount}
+
+  transactions.forEach(transaction => {
+    ul.innerHTML = `<li>Amount: ${transaction.amount}
        Description: ${transaction.description}
        Account: ${transaction.account}
        New Balance: ${transaction.newBalance}</li>`
-    })
+  })
 }
 displayTransactions()
 
-moveMoneyBtn.addEventListener("click", ()=>{
-  formWrap.style.display = "block";
+moveMoneyBtn.addEventListener("click", () => {
+  formWrapPayment.style.display = "block";
   moveMoneyBtn.style.display = "none"
 })
-closeBtn.addEventListener("click", ()=>{
-  formWrap.style.display = "none";
-  moveMoneyBtn.style.display = "block"
+closePaymentBtn.addEventListener("click", () => {
+  formWrapPayment.style.display = "none";
+  moveMoneyBtn.style.display = "inline-block"
+});
+
+addAccountBtn.addEventListener("click", () => {
+
+  formWrapAccount.style.display = "block"
+});
+closeAccountFormBtn.addEventListener("click", () => {
+  formWrapAccount.style.display = "none"
 })
 // to delete
 const tester = document.getElementById("tester")
 tester.addEventListener("click", () => {
-    console.log(transactions)
+  console.log(transactions)
 })
